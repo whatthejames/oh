9d6290f7e3011da67c7f18423375f11305d54849
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-03-13 02:07:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -76,3 +76,18 @@ bot.command("ask", async (ctx) => {
 
 
 bot.launch();
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
