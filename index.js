210832f45c11778eebd771e414b3cf7ef5a97ba4
whatthejames <124553067+whatthejames@users.noreply.github.com>
2023-03-13 10:15:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -57,3 +57,17 @@ function sendTextMessage(sender, text) {
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
 });
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
